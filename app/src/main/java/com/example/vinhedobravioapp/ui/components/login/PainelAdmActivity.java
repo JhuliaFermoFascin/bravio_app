1c75420e7b431c11b3a49c9a54098ac3a38cb99d
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2025-06-12 15:20:12 -0300

    start database on first time

--- app/src/main/java/com/example/vinhedobravioapp/ui/components/login/PainelAdmActivity.java
+++ app/src/main/java/com/example/vinhedobravioapp/ui/components/login/PainelAdmActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vinhedobravioapp.R;
 import com.example.vinhedobravioapp.components.CustomButtonComponent;
+import com.example.vinhedobravioapp.database.DPOpenHelper;
 import com.example.vinhedobravioapp.ui.components.vinhos.EstoqueActivity;
 
 public class PainelAdmActivity extends AppCompatActivity {
@@ -19,6 +20,9 @@ public class PainelAdmActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.painel_administrativo);
+
+        DPOpenHelper dbHelper = new DPOpenHelper(this); // 'this' é o contexto
+        dbHelper.getWritableDatabase(); // Isso garante a criação do banco/tabelas
 
         estoque_btn = findViewById(R.id.estoque_btn);
 
