1ef12accffedef8d29992a79077566f175173cff
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2025-06-16 17:30:00 -0300

    ajustes que precisei fazer pra rodar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.vinhedobravioapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.vinhedobravioapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
