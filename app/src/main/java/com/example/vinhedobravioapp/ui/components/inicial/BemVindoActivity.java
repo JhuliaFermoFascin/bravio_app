f6f6ca69328323067180d1c1fb48f4c682646a7d
JhuliaFermoFascin <jhuliafermo@gmail.com>
2025-06-17 17:11:20 -0300

    teste

--- app/src/main/java/com/example/vinhedobravioapp/ui/components/inicial/BemVindoActivity.java
+++ app/src/main/java/com/example/vinhedobravioapp/ui/components/inicial/BemVindoActivity.java
@@ -11,64 +11,54 @@ import com.example.vinhedobravioapp.components.CustomButtonComponent;
 import com.example.vinhedobravioapp.ui.components.helper.ConfirmacaoHelper;
 
 public class BemVindoActivity extends Activity {
-    private int origemAdm = 0;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_bem_vindo);
-    }
-    @Override
-    public void onBackPressed() {
-        View dialogView = getLayoutInflater().inflate(R.layout.modal_confirmacao, null);
+        private int origemAdm = 0;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.home_bem_vindo);
 
-        origemAdm = getIntent().getIntExtra(getString(R.string.tipo_usuario_input), 0);
+            origemAdm = getIntent().getIntExtra(getString(R.string.tipo_usuario_input), 0);
 
-        CustomButtonComponent btnVoltarMenu = findViewById(R.id.btnVoltar);
-        CustomButtonComponent btnVoltarDashboard = findViewById(R.id.btnVoltarDashboard);
+            CustomButtonComponent btnVoltarMenu = findViewById(R.id.btnVoltar);
+            CustomButtonComponent btnVoltarDashboard = findViewById(R.id.btnVoltarDashboard);
 
-        if (origemAdm == 0) {
-            btnVoltarDashboard.setVisibility(View.GONE);
+            if (origemAdm == 0) {
+                btnVoltarDashboard.setVisibility(View.GONE);
+            }
+
+            btnVoltarMenu.setOnClickListener(v -> {
+                mostrarConfirmacaoSaida(0);
+            });
+
+            btnVoltarDashboard.setOnClickListener(v -> {
+                mostrarConfirmacaoSaida(origemAdm);
+            });
         }
 
-        btnVoltarMenu.setOnClickListener(v -> {
-            mostrarConfirmacaoSaida(0);
-        });
+        private void mostrarConfirmacaoSaida(int destinoValor) {
+            String destinoTexto = (destinoValor == 1 || destinoValor == 2) ?
+                    getString(R.string.confirmar_retorno_adm) :
+                    getString(R.string.confirmar_retorno_menu);
 
-        btnVoltarDashboard.setOnClickListener(v -> {
+            String mensagem = getString(R.string.pergunta_saida, destinoTexto);
+
+            ConfirmacaoHelper.mostrarConfirmacao(this, mensagem, () -> {
+                if (destinoValor == 1) {
+                    Intent intent = new Intent(BemVindoActivity.this, DashboardAdmActivity.class);
+                    startActivity(intent);
+                }else if (destinoValor == 2){
+                    Intent intent = new Intent(BemVindoActivity.this, PainelRepresentanteActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    super.onBackPressed();
+                }
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            });
+        }
+        @Override
+        public void onBackPressed() {
             mostrarConfirmacaoSaida(origemAdm);
-        });
-    }
-
-    private void mostrarConfirmacaoSaida(int destinoValor) {
-        String destinoTexto = (destinoValor == 1 || destinoValor == 2) ?
-                getString(R.string.confirmar_retorno_adm) :
-                getString(R.string.confirmar_retorno_menu);
-
-        String mensagem = getString(R.string.pergunta_saida, destinoTexto);
-
-        ConfirmacaoHelper.mostrarConfirmacao(this, mensagem, () -> {
-            if (destinoValor == 1) {
-                Intent intent = new Intent(BemVindoActivity.this, DashboardAdmActivity.class);
-                startActivity(intent);
-            } else if (destinoValor == 2) {
-                Intent intent = new Intent(BemVindoActivity.this, PainelRepresentanteActivity.class);
-                startActivity(intent);
-            } else {
-                SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.preferencia_login), MODE_PRIVATE).edit();
-                editor.clear();
-                editor.apply();
-
-                Intent intent = new Intent(BemVindoActivity.this, MenuActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-            finish();
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        mostrarConfirmacaoSaida(origemAdm);
-    }
+        }
 }
